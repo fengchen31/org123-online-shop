91fb909729220cc1c0a2240c13186c66c6ea9923
FENG <feng@Mac.ht.home>
2025-12-09 00:10:13 -0500

    fix: 修正商品沒有被latest arrivals真正排序

--- components/facebook-style/collection-products-wrapper.tsx
+++ components/facebook-style/collection-products-wrapper.tsx
@@ -1,5 +1,6 @@
 import { getCollectionProducts, getCollection } from 'lib/shopify';
 import { CollectionProductsClient } from './collection-products-client';
+import { defaultSort } from 'lib/constants';
 
 interface CollectionProductsWrapperProps {
   collectionHandle: string;
@@ -9,7 +10,11 @@ export async function CollectionProductsWrapper({
   collectionHandle
 }: CollectionProductsWrapperProps) {
   const [products, collection] = await Promise.all([
-    getCollectionProducts({ collection: collectionHandle }),
+    getCollectionProducts({
+      collection: collectionHandle,
+      sortKey: defaultSort.sortKey,
+      reverse: defaultSort.reverse
+    }),
     getCollection(collectionHandle)
   ]);
 
